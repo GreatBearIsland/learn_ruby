d39148f209176a0bca6327d8dd702ea53e413a5a
GreatBearIsland <118938950+GreatBearIsland@users.noreply.github.com>
2023-05-08 15:45:39 +1200

    update bubble_sort

--- basic_ruby_projects/bubble_sort.rb
+++ basic_ruby_projects/bubble_sort.rb
@@ -3,8 +3,17 @@ def bubble_sort(array)
     sorted_array = []
     n = length.array
     #n.times do array.each_with_index
-    array.take(2) = evaluate_array
-    array.map  {|a| a < a+1 
-        if a > b
-        sorted_array.push()}
-    
+        array.take(2) = evaluate_array
+         array.map  {
+            if evaluate_array.at[0] > evaluate_array[1]
+                    evaluate_array.reverse
+                    sorted_array.push()
+            else sorted_array.push()
+            end}
+
+            p array 
+            p evaluate_array
+            p sorted_array
+end
+
+array[1,2,3]
