8e46b54122822df1bff7d8463627c8a4f1edeb0d
GreatBearIsland <118938950+GreatBearIsland@users.noreply.github.com>
2023-05-08 13:51:42 +1200

    add evaluate_array

--- basic_ruby_projects/bubble_sort.rb
+++ basic_ruby_projects/bubble_sort.rb
@@ -2,7 +2,9 @@ def bubble_sort(array)
     
     sorted_array = []
     n = length.array
-    n.times do array.each_with_index 
+    #n.times do array.each_with_index
+    array.take(2) = evaluate_array
+    array.map  {|a| a < a+1 
         if a > b
         sorted_array.push()}
     
