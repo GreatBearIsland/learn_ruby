83a186a92f618222bde7dc1a60f4edbc170cab0a
GreatBearIsland <118938950+GreatBearIsland@users.noreply.github.com>
2023-05-09 14:59:49 +1200

    cipher test

--- basic_ruby_projects/caesar_cipher.rb
+++ basic_ruby_projects/caesar_cipher.rb
@@ -1,3 +1,33 @@
-def caesar_cipher()
+def caesar_cipher(string, key)
+  cipher = [].to_str                      #ciphered array into a string
+  sentence = string.split(" ").to_a       #convert sentence to an array of words
+  alphabet = ('a'..'z').to_a          #convert alphabet range into an array
+  looped = false
   
+  sentence.each do |word|
+      n = word.length
+      for l in 0..n
+          alphabet.each_with_index do |a, i| 
+              if l = a 
+                  num = i + key
+                  if num > 26
+                     num = num - 26
+                     looped = true
+                  else
+                      looped = false
+                  end
+              letter = alphabet(num)
+              cipher.push(letter)
+              p i
+              p key
+              p num
+              p letter
+              p looped
+              p cipher
+              end
+          end
+      end
+      cipher.push(" ")
+      p cipher
+  end     
 end
