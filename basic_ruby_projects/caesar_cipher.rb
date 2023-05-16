123caec3154f16e7333ad8946dbcf17838f3d813
GreatBearIsland <118938950+GreatBearIsland@users.noreply.github.com>
2023-05-16 20:33:07 +1200

    update cipher

--- basic_ruby_projects/caesar_cipher.rb
+++ basic_ruby_projects/caesar_cipher.rb
@@ -1,33 +1,76 @@
+require 'pry-byebug'
+# def caesar_cipher(string, key)
+#     cipher = ()                       #ciphered string
+#     sentence = string.split(" ").to_a       #convert sentence to an array of words
+#     alphabet = ('a'..'z').to_a          #convert alphabet range into an array
+#     looped = false
+    
+#     sentence.each do |word|
+#         n = word.length
+#         for l in 0..n
+#             alphabet.each_with_index do |a, i| 
+#                 if l = a 
+#                     num = i + key
+#                     if num > 26
+#                        num = num - 26
+#                        looped = true
+#                     else
+#                         looped = false
+#                     end
+#                 letter = alphabet[num]
+#                 cipher.insert(-1, letter)
+#                 p i
+#                 p key
+#                 p num
+#                 p letter
+#                 p looped
+#                 p cipher
+#                 end
+#             end
+#         end
+#         cipher.push(" ")
+#         p cipher
+#     end     
+#   end
+#   caesar_cipher('I am a dog',1)
+
+
+
+
 def caesar_cipher(string, key)
-  cipher = [].to_str                      #ciphered array into a string
-  sentence = string.split(" ").to_a       #convert sentence to an array of words
-  alphabet = ('a'..'z').to_a          #convert alphabet range into an array
-  looped = false
-  
-  sentence.each do |word|
-      n = word.length
-      for l in 0..n
-          alphabet.each_with_index do |a, i| 
-              if l = a 
-                  num = i + key
-                  if num > 26
-                     num = num - 26
-                     looped = true
-                  else
-                      looped = false
-                  end
-              letter = alphabet(num)
-              cipher.push(letter)
-              p i
-              p key
-              p num
-              p letter
-              p looped
-              p cipher
-              end
-          end
-      end
-      cipher.push(" ")
-      p cipher
-  end     
-end
+    cipher = ""                          #ciphered string
+    alphabet = ('a'..'z').to_a              #convert alphabet range into an array
+    looped = false  
+
+    n = string.length
+    string.chars
+    for i in 0..n-1 do                         #assign each letter an index
+        if i == (" ")
+            cipher.insert(" ")
+            p cipher
+            binding.pry
+        else 
+            alphabet.each {|a| 
+                if i == a 
+                    num = i + key
+                    if num > 26
+                        num = num - 26
+                        looped = true
+                    else
+                        looped = false
+                    end
+                    letter = alphabet[num]
+                    cipher + letter
+                    p i
+                    p key
+                    p num
+                    p letter
+                    p looped
+                    p cipher
+                end}
+        end
+    end
+    puts "Expecting #{cipher}"
+end     
+
+caesar_cipher('I am a dog',1)
